0da41bab2c87ca7f9e34823d486e50179c2c956f
Matt Bailey <mbailey@pirho.com>
2014-03-02 18:30:49 -0700

    changed alert to console

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,8 +4,8 @@
 
 angular.module('myApp.controllers', ['asdf']).
 	controller('MyCtrl1', ['testService', function(testService) {
-		alert(testService.sayHello("World"));
+		console.dir(testService.sayHello("World"));
 	}]).
 	controller('MyCtrl2', [function() {
 
-}]);
+	}]);
