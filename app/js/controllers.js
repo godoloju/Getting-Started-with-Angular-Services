26e8f8308704eb16b5938ec2c185ebfe33a8f0ed
Matt Bailey <mbailey@pirho.com>
2014-03-02 18:24:48 -0700

    removed duplicate controller module

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,18 +2,10 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', ['asdf']).
+	controller('MyCtrl1', ['testService', function(testService) {
+		alert(testService.sayHello("World"));
+	}]).
+	controller('MyCtrl2', [function() {
 
-  }])
-  .controller('MyCtrl2', [function() {
-
-  }]);
-
-  angular.module('myApp.controllers', ['asdf']).
-    controller('MyCtrl1', ['testService', function(testService) {
-      alert(testService.sayHello("World"));
-  	}]).
-    controller('MyCtrl2', [function() {
-    	
-	}]);
+}]);
